55a0054445824d1aa6589a66b200c18930125b2f
Javakky <iemura.java@gmail.com>
2021-09-22 11:26:19 +0900

    Pipfile の利用

--- script/make-docs.sh
+++ script/make-docs.sh
@@ -1,7 +1,7 @@
-cd `dirname $0`/../
+# shellcheck disable=SC2164
 cd docs
+# shellcheck disable=SC2010
 ls -a | grep -v -E '^\.$|^\.\.$|^conf.py$|^index.rst$|^make.bat$|^Makefile$|^_static$' | xargs rm -rf
-
 cd ../
 sphinx-apidoc -f -e -o ./docs ./nicovideo_api_client
 cd docs
