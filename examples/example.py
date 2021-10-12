a75ec02b2a5b0b7b6e1ae63442ab62445cc49348
Javakky <iemura.java@gmail.com>
2021-10-12 13:05:13 +0900

    100件を超えるデータ取得のexample作成

--- examples/example.py
+++ examples/example.py
@@ -2,9 +2,10 @@ import sys
 
 sys.path.append(".")
 
-from examples import json_filter, version, simple
+from examples import json_filter, version, simple, multiple
 
 if __name__ == "__main__":
     simple.main()
     json_filter.main()
+    multiple.main()
     version.main()
