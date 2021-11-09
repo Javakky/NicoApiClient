0ec67db9b997a3ccbee12320a4005799e4461628
yashiroyrahs <ac214702@edu.okinawa-ct.ac.jp>
2021-11-09 22:52:15 +0900

    example.pyにexamples/simple_filter.pyを登録

--- examples/example.py
+++ examples/example.py
@@ -2,10 +2,11 @@ import sys
 
 sys.path.append(".")
 
-from examples import json_filter, version, simple, multiple
+from examples import json_filter, version, simple, simple_filter, multiple
 
 if __name__ == "__main__":
     simple.main()
+    simple_filter.main()
     json_filter.main()
     multiple.main()
     version.main()
