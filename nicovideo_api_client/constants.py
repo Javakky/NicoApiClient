fffd1f192ed1b8d4900cb9fbc6c32b62cd4c941d
Javakky <iemura.java@gmail.com>
2021-11-16 10:35:02 +0900

    FieldType にコメント追加

--- nicovideo_api_client/constants.py
+++ nicovideo_api_client/constants.py
@@ -4,25 +4,66 @@ from datetime import datetime
 
 
 class FieldType(Enum):
+    """コンテンツのフィールド種別"""
+
     CONTENT_ID = "contentId"
+    """コンテンツID
+    https://nico.ms/ の後に連結することでコンテンツへのURLになります
+    """
+
     TITLE = "title"
+    """タイトル"""
+
     DESCRIPTION = "description"
+    """コンテンツの説明文"""
+
     USER_ID = "userId"
+    """ユーザー投稿動画の場合、投稿者のユーザーID"""
+
     CHANNEL_ID = "channelId"
+    """	チャンネル動画の場合、チャンネルID"""
+
     VIEW_COUNTER = "viewCounter"
+    """再生数"""
+
     MYLIST_COUNTER = "mylistCounter"
+    """マイリスト数またはお気に入り数"""
+
     LIKE_COUNTER = "likeCounter"
+    """いいね！数"""
+
     LENGTH_SECONDS = "lengthSeconds"
+    """再生時間(秒)"""
+
     THUMBNAIL_URL = "thumbnailUrl"
+    """サムネイルのURL"""
+
     START_TIME = "startTime"
+    """コンテンツの投稿時間"""
+
     LAST_RES_BODY = "lastResBody"
+    """最新のコメント"""
+
     COMMENT_COUNTER = "commentCounter"
+    """コメント数"""
+
     LAST_COMMENT_TIME = "lastCommentTime"
+    """最終コメント時間"""
+
     CATEGORY_TAGS = "categoryTags"
+    """カテゴリタグ"""
+
     TAGS = "tags"
+    """タグ(空白区切り)"""
+
     TAGS_EXACT = "tagsExact"
+    """タグ完全一致(空白区切り)"""
+
     GENRE = "genre"
+    """ジャンル"""
+
     GENRE_KEYWORD = "genre.keyword"
+    """ジャンル完全一致"""
 
 
 DEFAULT_RETRY = 3
@@ -58,10 +99,6 @@ target_types = [
 ]
 
 
-class SortKeyType(Enum):
-    REGISTERED_AT = "registeredAt"
-
-
 MatchValue: TypeAlias = Union[List[str], List[int], List[datetime]]
 RangeLiteral: TypeAlias = Literal["gt", "gte", "lt", "lte"]
 RangeValue: TypeAlias = Union[
@@ -70,5 +107,6 @@ RangeValue: TypeAlias = Union[
 MatchDict: TypeAlias = Dict[FieldType, MatchValue]
 RangeDict: TypeAlias = Dict[FieldType, RangeValue]
 CombinedDict: TypeAlias = Dict[FieldType, Union[MatchValue, RangeValue]]
+
 JSON_LITERAL: TypeAlias = str | int | float | bool | None
 JSON: TypeAlias = JSON_LITERAL | Dict[str, "JSON"] | List["JSON"]
