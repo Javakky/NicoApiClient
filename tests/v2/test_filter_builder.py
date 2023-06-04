c43427ca755693065d7feb5339c3cefda709f97e
yashiroyrahs <84821621+yashiroyrahs@users.noreply.github.com>
2023-06-04 20:54:17 +0900

    フィルタビルダのテスト作成

---
+++ tests/v2/test_filter_builder.py
@@ -0,0 +1,29 @@
+import unittest
+
+from nicovideo_api_client.api.v2.filter_builder import FilterBuilder
+from nicovideo_api_client.constants import FieldType
+
+
+class FilterBuilderTestCase(unittest.TestCase):
+    @staticmethod
+    def test_field_nothing():
+        assert FilterBuilder().match_filter().range_filter() == {}
+
+    @staticmethod
+    def test_match_filter():
+        assert FilterBuilder().match_filter(FieldType.TITLE, "初音ミク") == {"title": "初音ミク"}
+
+    @staticmethod
+    def test_range_filter():
+        assert FilterBuilder().range_filter(FieldType.VIEW_COUNTER, "gt", 1000) == {"viewCounter": {"gt": 1000}}
+
+    @staticmethod
+    def test_range_filter_set():
+        assert FilterBuilder().range_filter(FieldType.VIEW_COUNTER, "gt", 1000, "lte", 5000) == {"viewCounter": {"gt": 1000, "lte": 5000}}
+
+    @staticmethod
+    def test_range_filter_sign():
+        assert FilterBuilder().range_filter(FieldType.VIEW_COUNTER, ">", 1000, "<=", 5000) == {"viewCounter": {"gt", 1000, "lte", 5000}}
+
+if __name__ == "__main__":
+    unittest.main()
