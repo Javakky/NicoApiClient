2423eaef13349819df5398f7760431608c061fbb
yashiroyrahs <84821621+yashiroyrahs@users.noreply.github.com>
2023-06-06 00:13:00 +0900

    長すぎる行の短縮

--- nicovideo_api_client/api/v2/filter_builder.py
+++ nicovideo_api_client/api/v2/filter_builder.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from typing import Dict, List, Union
 
-from nicovideo_api_client.constants import FieldType
+from nicovideo_api_client.constants import FieldType, MatchValue
 
 
 class FilterBuilder:
-    def __init__(self, filter: Dict[str, Union[Dict[str, Union[str, int, datetime]], Union[List[str], List[int], List[datetime]]]] = {}):
-        self.filter: Dict[str, Union[Dict[str, Union[str, int, datetime]], Union[List[str], List[int], List[datetime]]]] = filter
+    def __init__(self, filter: Dict[str, Union[Dict[str, Union[str, int, datetime]], MatchValue]] = {}):
+        self.filter: Dict[str, Union[Dict[str, Union[str, int, datetime]], MatchValue]] = filter
 
     def __cast_literal(self, literal: str) -> str:
         if literal != "gt" and literal != "gte" and literal != "lt" and literal != "lte":
@@ -51,13 +51,18 @@ class FilterBuilder:
         """
         if field_type is not None and literal1 is not None and value1 is not None:
             if literal2 is not None and value2 is not None:
-                self.filter[field_type.value] = {self.__cast_literal(literal1): value1, self.__cast_literal(literal2): value2}
+                self.filter[field_type.value] = {
+                    self.__cast_literal(literal1): value1,
+                    self.__cast_literal(literal2): value2,
+                }
             else:
                 self.filter[field_type.value] = {self.__cast_literal(literal1): value1}
 
         return self
 
-    def match_filter(self, field_type: FieldType = None, value: Union[List[str], List[int], List[datetime]] = None) -> "FilterBuilder":
+    def match_filter(
+        self, field_type: FieldType = None, value: Union[List[str], List[int], List[datetime]] = None
+    ) -> "FilterBuilder":
         """
         一致検索用の辞書を組み立てる
 
